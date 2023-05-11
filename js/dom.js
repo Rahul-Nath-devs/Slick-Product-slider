1c1290bc65867a46190299623fbb59dd76513a33
Rahul <123293982+Rahul-Nath-devs@users.noreply.github.com>
2023-05-11 08:22:58 -0700

    Add files via upload

---
+++ js/dom.js
@@ -0,0 +1,28 @@
+$(function(){
+
+    $('.main_body').slick({
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        arrows: true,
+        nextArrow:".arrows .rightArrow",
+        prevArrow:".arrows .leftArrow",
+        dots:true,
+        centerMode: true,
+        centerPadding: '',
+      
+
+
+    
+
+
+
+       
+        
+        
+      });
+ 
+
+
+
+
+})
